1967ddec357306be9704c402ded5fda31621e4c6
João Victor <jvictorcosta98@gmail.com>
2021-07-27 16:00:13 -0300

    Exercício 01

---
+++ ProjetoLivro/src/projetolivro/Pessoa.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projetolivro;
+
+/**
+ *
+ * @author Victor
+ */
+public class Pessoa {
+    private String nome;
+    private int idade;
+    private String sexo;
+
+    //CONSTRUTOR
+    public Pessoa(String nome, int idade, String sexo) {
+        this.nome = nome;
+        this.idade = idade;
+        this.sexo = sexo;
+    }
+    
+    //GETTERS E SETTERS
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public int getIdade() {
+        return idade;
+    }
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+    public String getSexo() {
+        return sexo;
+    }
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+    
+    //MÉTODOS ESPECIAIS
+    public void fazerAniver(){
+        this.setIdade(this.getIdade() + 1);
+    }    
+}
